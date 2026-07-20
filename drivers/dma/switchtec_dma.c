91a470052eec64f836c3ad60a3bd5be7431d4f9f
Logan Gunthorpe <logang@deltatee.com>
2026-07-20 13:37:08 -0600

    dmaengine: switchtec-dma: fix use-after-free of swdma_dev in remove()
    dma_async_device_unregister() can drop the last reference on dma_dev
    and free swdma_dev synchronously via switchtec_dma_release(), but
    switchtec_dma_remove() then uses swdma_dev->bar for iounmap().
    
    Cache bar in a local variable before the unregister call.
    
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Link: https://lore.kernel.org/dmaengine/20260717223431.625EE1F000E9@smtp.kernel.org
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- drivers/dma/switchtec_dma.c
+++ drivers/dma/switchtec_dma.c
@@ -1393,6 +1393,7 @@ err_disable:
 static void switchtec_dma_remove(struct pci_dev *pdev)
 {
 	struct switchtec_dma_dev *swdma_dev = pci_get_drvdata(pdev);
+	void __iomem *bar = swdma_dev->bar;
 
 	switchtec_dma_chans_release(pdev, swdma_dev);
 
@@ -1405,7 +1406,7 @@ static void switchtec_dma_remove(struct pci_dev *pdev)
 
 	dma_async_device_unregister(&swdma_dev->dma_dev);
 
-	iounmap(swdma_dev->bar);
+	iounmap(bar);
 	pci_release_mem_regions(pdev);
 	pci_disable_device(pdev);
 }
